40732333389eeae1540987b5e2fa45b682d7f43c
Jeremy <jeremy@jsrath.com>
2019-04-07 19:51:59 +0200

    feat: enable simultaneous slider and district filtering

--- src/app/components/events/events.component.ts
+++ src/app/components/events/events.component.ts
@@ -23,41 +23,46 @@ export class EventsComponent implements OnChanges {
   events: EventDetails[];
 
   @Input()
-	sort: string;
+  sort: string;
 
-	@Input()
-	district: DistrictCheckBoxEvent;
+  @Input()
+  district: DistrictCheckBoxEvent;
 
-	filteredEvents: EventDetails[];
+  filteredEvents: EventDetails[];
 
-	unchecked = [];
+  unchecked = [];
 
 
 
   ngOnChanges() {
     this.getData();
     this.events && this.applyFilters();
-		this.sort && this.sortEvents();
-		this.district && this.applyDistrictFilters();
+    this.sort && this.sortEvents();
+    this.district && this.applyDistrictFilters();
   }
 
   applyFilters() {
+    if (this.district) {
+      this.applyDistrictFilters();
+    }
     this.filteredEvents = this.events.filter(event => {
       let price = event.discount_price < this.price;
       let before = new Date(event.event_date).getHours() < this.beforeTime;
       let after = new Date(event.event_date).getHours() > this.afterTime;
-      return price && before && after;
+      let checked = !this.unchecked.includes(event.address.slice(1, 3));
+      if (this.unchecked) {
+        return checked && price && before && after;
+      } else {
+        return price && before && after
+      }
     })
-	}
+  }
 
-	applyDistrictFilters() {
-		!this.district.checked
-			? this.unchecked.push(this.district.district)
-			: this.unchecked = this.unchecked.filter(district => district !== this.district.district);
-
-			console.log(this.unchecked)
-			this.filteredEvents = this.events.filter(event => !this.unchecked.includes(event.address.slice(1,3)));
-	}
+  applyDistrictFilters() {
+    !this.district.checked
+      ? this.unchecked.push(this.district.district)
+      : this.unchecked = this.unchecked.filter(district => district !== this.district.district);
+  }
 
   getData() {
     this.eventService.getEvents().subscribe((events: ApiResponse) => {
@@ -66,6 +71,7 @@ export class EventsComponent implements OnChanges {
   }
 
   sortEvents() {
+    this.applyFilters();
     switch (this.sort) {
       case 'price':
         this.filteredEvents.sort((first, second) => first.discount_price - second.discount_price);
